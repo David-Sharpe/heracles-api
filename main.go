9c29fe811b713177c452c7f97a3b16544797a657
David-Sharpe <david.sharpe@careerbuilder.com>
2017-10-11 18:12:21 -0400

    Consolidate to main.go only.

--- main.go
+++ main.go
@@ -19,10 +19,17 @@ import (
 
 var db *pg.DB
 
+func getIDFromRequest(request *http.Request) int64 {
+    id, err := strconv.ParseInt(pat.Param(request, "id"), 10, 64)
+    if err != nil {
+        panic(err)
+    }
+    return id
+}
+
 func getWorkout(writer http.ResponseWriter, request *http.Request) {
-    id, _ := strconv.ParseInt(pat.Param(request, "id"), 10, 64)
     workout := workouts.Workout{
-        ID: id,
+        ID: getIDFromRequest(request),
     }
     workout.Read(db)
     res, _ := json.Marshal(workout)
@@ -78,6 +85,7 @@ func main() {
     db = pg.Connect(&pg.Options{
         User: "postgres",
         Password: os.Getenv("DB_PASSWORD"),
+        Database: "heracles"
     })
 
     mux := goji.NewMux()
