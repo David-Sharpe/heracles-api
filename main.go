d015f5735136d56627d601a2a0ea73d37943128c
USB Violator <username@users.noreply.github.com>
2025-12-26 14:39:39 -0500

    Move workout type to own file.

--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"heracles-api/domain"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!\n")
+		_, err := fmt.Fprintf(w, "Hello World!\n")
+		if err != nil {
+			return
+		}
 	})
-	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/workouts", func(w http.ResponseWriter, r *http.Request) {
+		workout := domain.Workout{Name: "Bench press"}
+		err := json.NewEncoder(w).Encode(workout)
+		if err != nil {
+			return
+		}
+	})
+
+	http.HandleFunc("/plans", func(w http.ResponseWriter, r *http.Request) {
+		workout := domain.Workout{Name: "Squat"}
+		if r.Method == "POST" {
+			err := json.NewEncoder(w).Encode(workout)
+			if err != nil {
+				return
+			}
+		} else {
+			workout.Name = "Bench"
+			err := json.NewEncoder(w).Encode(workout)
+			if err != nil {
+				return
+			}
+		}
+
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
